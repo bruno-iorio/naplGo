d9ca793838ec4cf77e7c755d9eba22ee8ecdb81f
bruno-iorio <bruno.fernandes.iorio@gmail.com>
2024-08-17 12:36:42 +0200

    new_play_match_function

--- Mechanics/board.c
+++ Mechanics/board.c
@@ -11,7 +11,6 @@ board* create_board(void){
   for(int i = 0; i < BOARDSIZE; i++){
     Board->grid[i] = (int *) malloc(BOARDSIZE*sizeof(int));
   }
-  
   for(int i = 0; i <  BOARDSIZE; i++){
     for(int j = 0; j <  BOARDSIZE; j++){
       if (i == 0 || i == BOARDSIZE - 1 || j == 0 || j == BOARDSIZE-1){
@@ -72,7 +71,6 @@ int find_group_liberties(board* Board,int* x, int* y,int x1,int y1, int* n){
 
   *(x + *n) = x1;
   *(y + *n) = y1;
-  printf("here it goes: %d , %d, %d\n",*(x+ *n),*(y+ *n ), *n);
   if(color1 == EMPTY || color2 == EMPTY || color3 == EMPTY || color4 == EMPTY){return 0;}
   
   if(color1 == color){
@@ -148,16 +146,7 @@ int play_match(board* Board){
       else{ 
         count++;
         memset(move,0,7);
-        switch (turn) {
-            case BLACK:
-              turn = WHITE;
-              break;
-            case WHITE:
-             turn = BLACK;
-             break;
-            default:
-              break;
-        }
+        turn = (turn == BLACK) ? WHITE : BLACK;
         continue;
       }
     }
@@ -180,9 +169,6 @@ int play_match(board* Board){
       for (int i = 0; i < 5; i++){n[i] = 0;}
       int color1,color2,color3,color4;
       color1 = Board->grid[x+1][y];
-      color2 = Board->grid[x-1][y];
-      color3 = Board->grid[x][y+1];
-      color4 = Board->grid[x][y-1];
       int captured_flag = 0;
       if(color1 != turn && color1 != WALL && color1 != EMPTY){
         if(find_group_liberties(Board,group_x[1],group_y[1],x+1,y,&n[1]) == 1){
@@ -190,18 +176,21 @@ int play_match(board* Board){
           captured_flag = 1;
         }
       }
+      color2 = Board->grid[x-1][y];
       if(color2 != turn && color2 != WALL && color2 != EMPTY){
         if(find_group_liberties(Board,group_x[2],group_y[2],x-1,y,&n[2]) == 1){
           capture_group(Board,group_x[2],group_y[2],n[2]);
           captured_flag = 1;
         }
       }
+      color3 = Board->grid[x][y+1];
       if(color3 != turn && color3 != WALL && color3 != EMPTY){
         if(find_group_liberties(Board,group_x[3],group_y[3],x,y+1,&n[3]) == 1){
           capture_group(Board,group_x[3],group_y[3],n[3]);
           captured_flag = 1;
         }
       }
+      color4 = Board->grid[x][y-1];
       if(color4 != turn && color4 != WALL && color4 != EMPTY){
         if(find_group_liberties(Board,group_x[4],group_y[4],x,y-1,&n[4]) == 1){
           capture_group(Board,group_x[4],group_y[4],n[4]);
@@ -220,16 +209,7 @@ int play_match(board* Board){
         printf("invalid move: ko\n");
         continue;
       }
-      switch (turn) {
-          case BLACK:
-            turn = WHITE;
-            break;
-          case WHITE:
-            turn = BLACK;
-            break;
-          default:
-            break;
-      }
+      turn = (turn == BLACK) ? WHITE : BLACK;
       
       print_board(Board);
       copy_board(copy2,copy1,BOARDSIZE);
@@ -259,4 +239,195 @@ void copy_board(int** grid2, int** grid1, int n){
       grid2[i][j] = grid1[i][j];
   }
 }
+}
+
+int play_move_out_of_match(board* Board,grid* copy1, grid* copy2, int* turn,int *pass_count,int *count,char move[7],int* Ko_check){
+  int x, y;
+  if(!strncmp(move,"pass",4)){
+      print_board(Board);
+      (*pass_count)++;
+      (*count)++;
+      if(*pass_count == 2){
+        return 1; //game over;
+    }
+    memset(move,0,7);
+    (*count)++;
+    *turn = (*turn == BLACK) ? WHITE : BLACK; 
+    return 0;
+    }
+    sscanf(move,"%d %d",&x,&y);
+    if(x <= 0 || x >= BOARDSIZE-1 || y <= 0 || y >= BOARDSIZE-1){
+      printf("Invalid input\n");
+      memset(move,0,7);
+    return 5; // input out of board.
+    }
+
+  if(Board->grid[x][y] ==EMPTY){
+    play_move(Board,*turn,x,y);
+    int n[5];
+    int group_x[5][361], group_y[5][361]; 
+    for (int i = 0; i < 5; i++){n[i] = 0;}
+    int color1,color2,color3,color4;
+    color1 = Board->grid[x+1][y];
+    int captured_flag = 0;
+    if(color1 != *turn && color1 != WALL && color1 != EMPTY){
+      if(find_group_liberties(Board,group_x[1],group_y[1],x+1,y,&n[1]) == 1){
+        capture_group(Board,group_x[1],group_y[1],n[1]);
+        captured_flag = 1;
+      }
+    }
+    color2 = Board->grid[x-1][y];
+    if(color2 != *turn && color2 != WALL && color2 != EMPTY){
+      if(find_group_liberties(Board,group_x[2],group_y[2],x-1,y,&n[2]) == 1){
+        capture_group(Board,group_x[2],group_y[2],n[2]);
+        captured_flag = 1;
+      }
+    }
+    color3 = Board->grid[x][y+1];
+    if(color3 != *turn && color3 != WALL && color3 != EMPTY){
+      if(find_group_liberties(Board,group_x[3],group_y[3],x,y+1,&n[3]) == 1){
+        capture_group(Board,group_x[3],group_y[3],n[3]);
+        captured_flag = 1;
+      }
+    }
+    color4 = Board->grid[x][y-1];
+    if(color4 != *turn && color4 != WALL && color4 != EMPTY){
+      if(find_group_liberties(Board,group_x[4],group_y[4],x,y-1,&n[4]) == 1){
+        capture_group(Board,group_x[4],group_y[4],n[4]);
+        captured_flag = 1;
+      }
+    }
+    if(find_group_liberties(Board,group_x[0],group_x[0],x,y,&n[0]) == 1){
+      printf("invalid move\n");
+      play_move(Board,EMPTY,x,y);
+      memset(move,0,7);
+      return 3; //suicide without caputre;
+    }
+    memset(move,0,7);
+    if(*Ko_check == 1 && captured_flag == 1){  
+      if(!compare_boards(Board->grid,copy2->tab,BOARDSIZE)&& *count > 1){
+        copy_board(Board->grid,copy1->tab,BOARDSIZE);
+        printf("invalid move: ko\n");
+        return 2; //invlid for ko
+      }
+    } 
+    *pass_count = 0;
+    *Ko_check = (captured_flag == 1) ? 1 : 0;
+    *turn = (*turn == BLACK) ? WHITE : BLACK ; 
+    print_board(Board);
+    copy_board(copy2->tab,copy1->tab,BOARDSIZE);
+    copy_board(copy1->tab,Board->grid,BOARDSIZE);
+    (*count)++;
+  }
+  return 0;
+}
+
+void list_possible_moves(int* x_list,int* y_list,int* numb, board* Board,grid* copy1, grid* copy2, int turn,int count,int Ko_check){
+  for(int x = 1; x < BOARDSIZE-1; x++){
+    for(int y = 1; y < BOARDSIZE-1; y++){
+      if(Board->grid[x][y] ==EMPTY){
+      play_move(Board,turn,x,y);
+
+      int n[5];
+      int group_x[5][361], group_y[5][361]; 
+
+      for (int i = 0; i < 5; i++){n[i] = 0;}
+ 
+      int color1,color2,color3,color4;
+ 
+      color1 = Board->grid[x+1][y];
+      int captured_flag = 0;
+
+      if(color1 != turn && color1 != WALL && color1 != EMPTY){
+        if(find_group_liberties(Board,group_x[1],group_y[1],x+1,y,&n[1]) == 1){
+          capture_group(Board,group_x[1],group_y[1],n[1]);
+          captured_flag = 1;
+        }
+      }
+      color2 = Board->grid[x-1][y];
+      if(color2 != turn && color2 != WALL && color2 != EMPTY){
+        if(find_group_liberties(Board,group_x[2],group_y[2],x-1,y,&n[2]) == 1){
+          capture_group(Board,group_x[2],group_y[2],n[2]);
+          captured_flag = 1;
+        }
+      }
+      color3 = Board->grid[x][y+1];
+      if(color3 != turn && color3 != WALL && color3 != EMPTY){
+        if(find_group_liberties(Board,group_x[3],group_y[3],x,y+1,&n[3]) == 1){
+          capture_group(Board,group_x[3],group_y[3],n[3]);
+          captured_flag = 1;
+        }
+      }
+      color4 = Board->grid[x][y-1];
+      if(color4 != turn && color4 != WALL && color4 != EMPTY){
+        if(find_group_liberties(Board,group_x[4],group_y[4],x,y-1,&n[4]) == 1){
+          capture_group(Board,group_x[4],group_y[4],n[4]);
+          captured_flag = 1;
+        }
+      }
+      if(find_group_liberties(Board,group_x[0],group_x[0],x,y,&n[0]) == 1){
+        play_move(Board,EMPTY,x,y);
+        continue;
+      }
+      if(Ko_check == 1 && captured_flag == 1){  
+        if(!compare_boards(Board->grid,copy2->tab,BOARDSIZE)&& count > 1){
+          copy_board(Board->grid,copy1->tab,BOARDSIZE);
+          continue;
+       //invlid for ko
+        }
+      } 
+    play_move(Board,EMPTY,x,y); //remove the initial move
+    x_list[*numb] = x;
+    y_list[*numb] = y;
+    (*numb)++;
+      } 
+    }
+  }
+}
+
+
+
+int main(){
+  board* Board = create_board();
+  
+  grid copy1;
+  grid copy2;
+  
+  copy1.tab = (int**) malloc(sizeof(int*)*BOARDSIZE);  
+  copy2.tab = (int**) malloc(sizeof(int*)*BOARDSIZE);  
+  
+  for (int i = 0; i < BOARDSIZE; i++) {
+    copy1.tab[i] = (int*) malloc(sizeof(int)*BOARDSIZE);  
+    copy2.tab[i]= (int*) malloc(sizeof(int)*BOARDSIZE);  
+  }
+  copy_board(copy1.tab,Board->grid,BOARDSIZE);
+  copy_board(copy2.tab,Board->grid,BOARDSIZE);
+  int turn = BLACK;
+  int pass_count = 0, count = 0;
+  int Ko_check = 0;
+  char move[7];
+  int n=0;
+  int* x = (int*) malloc(sizeof(int)*361);
+  int* y = (int*) malloc(sizeof(int)*361);
+  
+  while(fgets(move,7,stdin)){
+    list_possible_moves(x,y,&n,Board,&copy1,&copy2,turn,count,Ko_check);
+    printf("The available moves are: \n");
+    printf("%d",n);
+    for(int j = 0; j < n;j++) {printf("x: %d, y: %d\n",x[j],y[j]);}
+    n = 0;
+    int i = play_move_out_of_match(Board,&copy1,&copy2,&turn,&pass_count,&count,move,&Ko_check);
+    if (i == 1){
+      printf("Game is over\n");
+      break;
+    }
+  }
+  free(Board);
+  for(int i = 0; i < BOARDSIZE ; i++){
+    free(copy1.tab[i]);
+    free(copy2.tab[i]);
+  }
+  free(copy1.tab);
+  free(copy2.tab);
+  return 0;
 }
